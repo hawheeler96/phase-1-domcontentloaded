76110e68a136ee4dc693e748cb0874218657b7c7
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-11-01 14:42:10 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    const repText = document.getElementById('text');
+    repText.textContent = 'This is really cool!'
+})
